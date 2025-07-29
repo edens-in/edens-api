2ad532a11eeb152fcf37244ea8ee9571855f3231
Soumik Ghosh <132730107+SoumikGhosh111@users.noreply.github.com>
2025-07-29 10:48:13 +0530

    commented out one line for deployment
    try to implement the controllers before implementing routes

--- routes/estimate.js
+++ routes/estimate.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const getDelhiveryEstimate = require('../controllers/estimateController');
+// const getDelhiveryEstimate = require('../controllers/estimateController');
 
 router.post('/', getDelhiveryEstimate);
 
